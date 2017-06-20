12c323224307fd2002bfbefa46bec1c47942a983
Thomas Nabord <thomas.nabord@prestashop.com>
2017-06-20 08:59:41 +0100

    Main class cleanup

--- firebaseauthenticator.php
+++ firebaseauthenticator.php
@@ -24,8 +24,6 @@
  *  International Registered Trademark & Property of PrestaShop SA
  */
 
-require_once(dirname(__FILE__).'/classes/FirebaseClient.php');
-
 class FirebaseAuthenticator extends Module
 {
     public function __construct()
@@ -38,17 +36,15 @@ class FirebaseAuthenticator extends Module
         $this->bootstrap = true;
         parent::__construct();
 
-        $this->displayName = $this->trans('Hooks for admin login page', array(), 'Modules.Autoupgrade.Admin');
-        $this->description = $this->trans('Provides hooks to login your shop.', array(), 'Modules.Autoupgrade.Admin');
+        $this->displayName = $this->l('Firebase Authenticator');
+        $this->description = $this->l('Handle BO authentication with Firebase.');
 
         //$this->ps_versions_compliancy = array('min' => '1.7.2.0', 'max' => _PS_VERSION_);
     }
 
     public function install()
     {
-        parent::install();
-        $this->registerHook('actionAdminLoginControllerSetMedia');
-        return true;
+        return (parent::install() && $this->registerHook('actionAdminLoginControllerSetMedia'));
     }
 
     public function hookActionAdminLoginControllerSetMedia()
